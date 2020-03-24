b0d5f30cc8a8e8602f54dcce21f7f454f34ff196
Timtim <timkrow@centurylink.net>
2020-03-23 21:09:15 -0400

    Initial commit

---
+++ src/Fighter.java
@@ -0,0 +1,7 @@
+
+public class Fighter {
+
+	public void atack() {
+		System.out.println("attacking");
+	}
+}
---
+++ src/Player.java
@@ -0,0 +1,7 @@
+
+public class Player {
+
+	public void atack() {
+		System.out.println("attacking");
+	}
+}
